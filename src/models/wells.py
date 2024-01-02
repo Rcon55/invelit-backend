bccd0aceedc57533a0cf48ce8dfcd1632c148413
Roman Osipenko <rcon55@gmail.com>
2024-01-02 13:06:17 +0500

    change db structure

--- src/models/wells.py
+++ src/models/wells.py
@@ -1,25 +1,52 @@
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Float
 from sqlalchemy.sql import func
 
 from src.models.base import Base
 
 
+class Datasets(Base):
+    __tablename__ = "Datasets"
+    id = Column(Integer, primary_key=True)
+    dataset = Column(Integer, nullable=False)
+    name = Column(String(30))
+    well = Column(Integer, ForeignKey("Wells.well"))
+
+
 class Wells(Base):
     __tablename__ = "Wells"
-    well_id = Column(Integer, primary_key=True)
-    well_name = Column(String(30), nullable=False)
-    field_id = Column(Integer, ForeignKey("Fields.field_id"))
-    time_created = Column(DateTime(timezone=True), server_default=func.now())
+    well = Column(Integer, primary_key=True)
+    name = Column(String(30))
+    field = Column(Integer, ForeignKey("Fields.field_id"))
 
 
-class Boreholes(Base):
-    __tablename__ = "Boreholes"
-    borehole_id = Column(Integer, primary_key=True)
-    borehole_name = Column(String(30), nullable=False)
-    well_id = Column(Integer, ForeignKey("Wells.well_id"))
+class Samples(Base):
+    __tablename__ = "Samples"
+    sample = Column(Integer, primary_key=True)
+    name = Column(String(30))
+    well = Column(Integer, ForeignKey("Wells.well"))
+    md = Column(Integer)
 
 
-class Fields(Base):
-    __tablename__ = "Fields"
-    field_id = Column(Integer, primary_key=True)
-    field_name = Column(String(30), nullable=False)
+class Experiments(Base):
+    __tablename__ = "Experiments"
+    experiment = Column(Integer, primary_key=True)
+    sample = Column(Integer, ForeignKey("Samples.sample"))
+    name = Column(String(30))
+    type = Column(Integer, ForeignKey("Dict.id"))
+    date = Column(DateTime(timezone=True))
+
+
+class Properties(Base):
+    __tablename__ = "Properties"
+    id = Column(Integer, primary_key=True)
+    experiment = Column(Integer, ForeignKey("Experiments.experiment"))
+    property = Column(Integer, ForeignKey("Dict.id"))
+    value = Column(Float)
+
+
+class Dict(Base):
+    __tablename__ = "Dict"
+    id = Column(Integer, primary_key=True)
+    group = Column(Integer)
+    name = Column(String(30))
+    description = Column(String(400))
